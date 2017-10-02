3a4515f175f50ea66ab6cb33c2730bc38031b9b2
unknown <ahemaid@eis-haltern.iais.fraunhofer.de>
2017-10-02 08:46:52 +0200

    fix SKOS parent clicks

--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,11 @@
 var express = require('express');
 var router = express.Router();
-var app = express();
-
-
-//  GET home page. 
-//app.get('/', function (req, res) {
-
+//  GET home page.
 router.get('/', function (req, res) {
-
-    var url = req.url;
-    console.log(url);
-
-    var template = '<a href="http://localhost:3000/">YourURL is ' + ur + '. </a>';
-
-    return res.send(template);
-    // GET home page. 
-    /* res.render('index', {
+    // GET home page.
+    res.render('index', {
          title: 'MobiVoc',
-         concept: url
-     });*/
+     });
 });
 
 module.exports = router;
