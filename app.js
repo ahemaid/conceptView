b2d8a5b27c1620917cb504b55b845140a1e43211
Hemid <ahmedcertsoft@gmail.com>
2017-10-20 13:59:54 +0200

    fix fileMenu of skos

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ var users = require('./routes/users');
 var tree = require('./routes/tree');
 var visualization = require('./routes/visualization');
 var turtleEditor = require('./routes/turtleEditor');
+var config = require('./routes/config');
+var fs = require('fs');
+var  jsonfile  =  require('jsonfile')
 
 
 //var selectServices = require('./routes/selectServices');
@@ -34,46 +37,13 @@ app.set('view engine', 'ejs');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-    extended: true
+  extended: true
 }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-
-//app.use('jQuery', express.static(__dirname + '/node_modules/jquery/dist/'));
-
-var data = require('./RDFresults');
-
-// TO:DO check if the file's expetions
-function SortByName(x, y) {
-    return ((x.concept.toLowerCase() == y.concept.toLowerCase()) ? 0 : ((x.concept.toLowerCase() > y.concept.toLowerCase()) ? 1 : -1));
-}
-
-function uniqurArray(array) {
-    var out = [];
-    var sl = array;
-
-    for (var i = 0, l = sl.length; i < l; i++) {
-        var unique = true;
-        for (var j = 0, k = out.length; j < k; j++) {
-            if (sl[i].concept.toLowerCase() === out[j].concept.toLowerCase()) {
-                unique = false;
-            }
-        }
-        if (unique) {
-            out.push(sl[i]);
-        }
-    }
-
-    return out;
-}
-
-// Call Sort By Name
-data.sort(SortByName);
-app.locals.appdata = uniqurArray(data);
-//console.log(app.locals.appdata);
+// routing to the available routes on the app
 app.use('/', routes);
-
 app.use('/contactus', contactus);
 app.use('/users', users);
 app.use('/tree', tree);
@@ -81,44 +51,98 @@ app.use('/visualization', visualization);
 app.use('/webvowlLink', express.static("views/webvowl"));
 app.use('/turtleEditorLink', express.static("views/turtleEditor"));
 app.use('/turtleEditor', turtleEditor);
+app.use('/config', config);
 
-
-
-
-
-//  catch 404 and forward to error handler
-app.use(function (req, res, next) {
-    console.log(req.url);
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+// http post when  a user configurations is submitted
+app.post('/config', function(req, res) {
+  var timeout = null;
+  var filepath = 'config.json';
+  var emptyJSONFile = false;
+  var userConfigurationsArray = [];
+  // Read the userConfigurations file if exsit to append new data
+  jsonfile.readFile(filepath, function(err, obj)  {
+    if (err)
+      console.log(err);  
+    if (isEmptyObject(obj)) {
+      emptyJSONFile = true;
+    } else {
+      if (obj.length > 0) {//push if there are array of objects
+        for (var i = 0; i < obj.length; i++)
+          userConfigurationsArray.push(obj[i]);
+      }
+      // push the current enterned object
+      userConfigurationsArray.push(req.body);
+      //used to write user configurations to the predefined file
+      jsonfile.writeFile(filepath,  userConfigurationsArray,   {
+        spaces:  2,
+         EOL:   '\r\n'
+      },  function(err)  {  
+        if (err)
+          throw err;
+      })
+    }
+  })
+  // for the first time if file is empty or not exsit
+  var arrayForFirstTime =[];
+  arrayForFirstTime.push(req.body);
+  timeout = setTimeout(function() {
+    if (emptyJSONFile) {
+      jsonfile.writeFile(filepath,  arrayForFirstTime,   {
+        spaces:  2,
+         EOL:   '\r\n'
+      },  function(err)  {  
+        if (err)
+          throw err;
+        console.log("write");
+      })
+    }
+  }, 3000);
+  // redirect to another page
+  res.render('userConfigurationsUpdated', {
+    title: 'System is making ready'
+  });
 });
 
-/// error handlers
+function isEmptyObject(obj) {
+  for (var key in obj) {
+    if (Object.prototype.hasOwnProperty.call(obj, key)) {
+      return false;
+    }
+  }
+  return true;
+}
 
+//  catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  console.log(req.url);
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
 // development error handler
 // will print stacktrace
-
 if (app.get('env') === 'development') {
-    app.use(function (err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err,
-            title: 'error'
-        });
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err,
+      title: 'error'
     });
+  });
 }
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function (err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {},
-        title: 'error'
-    });
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {},
+    title: 'error'
+  });
 });
 
 
---
+++ routes/config.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+var app = express();
+var bodyParser = require('body-parser');
+//  GET home page.
+router.get('/', function(req, res) {
+  router.use(bodyParser.urlencoded({
+    extended: false
+  }));
+
+
+  var str = 'notYet';
+  // GET home page.
+  res.render('config', {
+    title: 'Configuration Page'
+  });
+});
+
+module.exports = router;
