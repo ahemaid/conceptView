b114089503c976e3c05cd1e9ac43459b253d9873
Hemid <ahmedcertsoft@gmail.com>
2017-10-24 11:55:46 +0200

    add configuration page at the first time when run the application

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,28 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
 //  GET home page.
 router.get('/', function (req, res) {
-    // GET home page.
-    res.render('index', {
-         title: 'MobiVoc',
-     });
+
+  // check if the userConfigurations file is exist
+  // for the first time of app running
+  var path = "jsonDataFiles/userConfigurations.json";
+  fs.exists(path, function(exists) {
+    if(!exists){
+      console.log("dsfdfdsfdsf                 dfdfsdf d         dffdsf"+path + " exists? " + exists);
+      res.redirect('/config');
+
+    }
+    else {
+      {
+        //GET home page.
+        res.render('index', {
+             title: 'MobiVoc',
+         });
+      }
+    }
+  });
+
 });
 
 module.exports = router;
